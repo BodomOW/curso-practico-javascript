986744dfde1aa47a267a8b87d1202abaee4f6042
Marco Hernandez <mh94.design@gmail.com>
2021-11-03 12:20:42 -0600

    Mediana convertida en funcion y su lista ordenada de menor a mayor con metodo sort

--- mediana.js
+++ mediana.js
@@ -8,27 +8,31 @@ function calcularMediaAritmetica(lista) {
   return promedioLista;
 }
 
-const lista1 = [100, 200, 500, 40000000000];
+function calcularMediana(lista) {
+  lista.sort((a, b) => a - b);
 
-const mitadLista1 = parseInt(lista1.length / 2);
+  const mitadLista = parseInt(lista.length / 2);
 
-function esPar(numerito) {
-  if (numerito % 2 === 0) {
-    return true;
-  } else {
-    return false;
+  function esPar(numerito) {
+    if (numerito % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
+  let mediana;
 
-let mediana;
+  if (esPar(lista.length)) {
+    const elemento1 = lista[mitadLista - 1];
+    const elemento2 = lista[mitadLista];
 
-if (esPar(lista1.length)) {
-  const elemento1 = lista1[mitadLista1 - 1];
-  const elemento2 = lista1[mitadLista1];
+    const promedio1y2 = calcularMediaAritmetica([elemento1, elemento2]);
 
-  const promedio1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    mediana = promedio1y2;
 
-  mediana = promedio1y2;
-} else {
-  mediana = lista1[mitadLista1];
+    return `La lista ordenada es: ${lista} y su mediana es: ${mediana}`;
+  } else {
+    mediana = lista[mitadLista];
+    return `La lista ordenada es: ${lista} y su mediana es: ${mediana}`;
+  }
 }
