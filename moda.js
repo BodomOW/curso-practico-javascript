3b1d06d543c82137547de77e152b68694fae6d98
Bodom <mh94.design@gmail.com>
2021-11-05 10:08:30 -0600

    Moda convertida en funcion

--- moda.js
+++ moda.js
@@ -1,21 +1,25 @@
-const lista1 = [1, 2, 3, 1, 2, 3, 2, 2, 2, 2, 3, 1, 2, 3];
+let lista = [];
 
-const lista1Count = {};
+function calcularModa(lista) {
+    const listaCount = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else {
-            lista1Count[elemento] = 1;
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
+    );
+    
+    const listaArray = Object.entries(listaCount).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];       
     }
-);
+    )
+    
+    const moda = listaArray[listaArray.length - 1];
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1] - elementoB[1];       
+    return moda;
 }
-)
-
-const moda = lista1Array[lista1Array.length - 1]
